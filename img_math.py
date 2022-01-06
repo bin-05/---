d67bb7cdce8957585d0a6cd2827d00580fed63cb
bin-05 <52254194+bin-05@users.noreply.github.com>
2022-01-06 17:08:07 +0800

    排除面积最小的点

--- img_math.py
+++ img_math.py
@@ -1,1 +1,85 @@
 
+import cv2
+import numpy as np
+
+MAX_WIDTH = 1000
+Min_Area = 2000
+SZ = 20
+PROVINCE_START = 1000
+"""
+该文件包含读文件函数
+取零值函数
+矩阵校正函数
+颜色判断函数
+"""
+
+
+def img_read(filename):
+    return cv2.imdecode(np.fromfile(filename, dtype=np.uint8), cv2.IMREAD_COLOR)
+    # 以uint8方式读取filename 放入imdecode中，cv2.IMREAD_COLOR读取彩色照片
+
+
+
+
+def point_limit(point):
+    if point[0] < 0:
+        point[0] = 0
+    if point[1] < 0:
+        point[1] = 0
+
+
+def accurate_place(card_img_hsv, limit1, limit2, color):
+    row_num, col_num = card_img_hsv.shape[:2]
+    xl = col_num
+    xr = 0
+    yh = 0
+    yl = row_num
+    row_num_limit = 21
+    col_num_limit = col_num * 0.8 if color != "green" else col_num * 0.5  # 绿色有渐变
+    for i in range(row_num):
+        count = 0
+        for j in range(col_num):
+            H = card_img_hsv.item(i, j, 0)
+            S = card_img_hsv.item(i, j, 1)
+            V = card_img_hsv.item(i, j, 2)
+            if limit1 < H <= limit2 and 34 < S and 46 < V:
+                count += 1
+        if count > col_num_limit:
+            if yl > i:
+                yl = i
+            if yh < i:
+                yh = i
+    for j in range(col_num):
+        count = 0
+        for i in range(row_num):
+            H = card_img_hsv.item(i, j, 0)
+            S = card_img_hsv.item(i, j, 1)
+            V = card_img_hsv.item(i, j, 2)
+            if limit1 < H <= limit2 and 34 < S and 46 < V:
+                count += 1
+        if count > row_num - row_num_limit:
+            if xl > j:
+                xl = j
+            if xr < j:
+                xr = j
+    return xl, xr, yh, yl
+
+
+def img_findContours(img_contours):
+    contours, hierarchy = cv2.findContours(img_contours, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = [cnt for cnt in contours if cv2.contourArea(cnt) > Min_Area]
+    print("findContours len = ", len(contours))
+    # 排除面积最小的点
+    car_contours = []
+    for cnt in contours:
+        ant = cv2.minAreaRect(cnt)
+        width, height = ant[1]
+        if width < height:
+            width, height = height, width
+        ration = width / height
+
+        if 2 < ration < 5.5:
+            car_contours.append(ant)
+            box = cv2.boxPoints(ant)
+
+    return car_contours

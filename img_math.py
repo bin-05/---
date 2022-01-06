1bbacbec99527104b6d78630ebf3e7ef9a232972
bin-05 <52254194+bin-05@users.noreply.github.com>
2022-01-06 17:10:23 +0800

    根据设定的阈值和图片直方图，找出波峰，用于分隔字符

--- img_math.py
+++ img_math.py
@@ -219,3 +219,32 @@ def img_color(card_imgs):
 
     return colors, card_imgs
 
+# 根据设定的阈值和图片直方图，找出波峰，用于分隔字符
+def find_waves(threshold, histogram):
+    up_point = -1  # 上升点
+    is_peak = False
+    if histogram[0] > threshold:
+        up_point = 0
+        is_peak = True
+    wave_peaks = []
+    for i, x in enumerate(histogram):
+        if is_peak and x < threshold:
+            if i - up_point > 2:
+                is_peak = False
+                wave_peaks.append((up_point, i))
+        elif not is_peak and x >= threshold:
+            is_peak = True
+            up_point = i
+    if is_peak and up_point != -1 and i - up_point > 4:
+        wave_peaks.append((up_point, i))
+    return wave_peaks
+
+#分离车牌字符
+def seperate_card(img, waves):
+    part_cards = []
+    for wave in waves:
+        part_cards.append(img[:, wave[0]:wave[1]])
+
+    return part_cards
+
+

6a91ec0828a280914b265365bc0f48269994d8ba
bin-05 <52254194+bin-05@users.noreply.github.com>
2022-01-06 17:04:21 +0800

    根据输入的图像进行图像预处理

--- img_function.py
+++ img_function.py
@@ -1,1 +1,141 @@
+import os
+import cv2
+import numpy as np
+import show
+import img_math
+import img_recognition
+import transform
 
+
+SZ = 20  # 训练图片长宽
+MAX_WIDTH = 1000  # 原始图片最大宽度
+Min_Area = 2000  # 车牌区域允许最大面积
+PROVINCE_START = 1000
+
+
+class StatModel(object):
+    def load(self, fn):
+        self.model = self.model.load(fn)
+
+    def save(self, fn):
+        self.model.save(fn)
+
+
+class SVM(StatModel):
+    def __init__(self, C=1, gamma=0.5):
+        self.model = cv2.ml.SVM_create()
+        self.model.setGamma(gamma)
+        self.model.setC(C)
+        self.model.setKernel(cv2.ml.SVM_RBF)
+        self.model.setType(cv2.ml.SVM_C_SVC)
+
+    # 训练svm
+    def train(self, samples, responses):
+        self.model.train(samples, cv2.ml.ROW_SAMPLE, responses)
+
+    # 字符识别
+    def predict(self, samples):
+        r = self.model.predict(samples)
+        return r[1].ravel()
+
+
+class CardPredictor:
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        self.save_traindata()
+
+    def train_svm(self):
+        # 识别英文字母和数字
+        self.model = SVM(C=1, gamma=0.5)
+        # 识别中文
+        self.modelchinese = SVM(C=1, gamma=0.5)
+        if os.path.exists("svm.dat"):
+            self.model.load("svm.dat")
+        else:
+            chars_train = []
+            chars_label = []
+
+            for root, dirs, files in os.walk("train\\chars2"):
+                if len(os.path.basename(root)) > 1:
+                    continue
+                root_int = ord(os.path.basename(root))
+                for filename in files:
+                    filepath = os.path.join(root, filename)
+                    digit_img = cv2.imread(filepath)
+                    digit_img = cv2.cvtColor(digit_img, cv2.COLOR_BGR2GRAY)
+                    chars_train.append(digit_img)
+                    # chars_label.append(1)
+                    chars_label.append(root_int)
+
+            chars_train = list(map(img_recognition.deskew, chars_train))
+            chars_train = img_recognition.preprocess_hog(chars_train)
+            # chars_train = chars_train.reshape(-1, 20, 20).astype(np.float32)
+            chars_label = np.array(chars_label)
+            print(chars_train.shape)
+            self.model.train(chars_train, chars_label)
+        if os.path.exists("svmchinese.dat"):
+            self.modelchinese.load("svmchinese.dat")
+        else:
+            chars_train = []
+            chars_label = []
+            for root, dirs, files in os.walk("train\\charsChinese"):
+                if not os.path.basename(root).startswith("zh_"):
+                    continue
+                pinyin = os.path.basename(root)
+                index = img_recognition.provinces.index(pinyin) + PROVINCE_START + 1  # 1是拼音对应的汉字
+                for filename in files:
+                    filepath = os.path.join(root, filename)
+                    digit_img = cv2.imread(filepath)
+                    digit_img = cv2.cvtColor(digit_img, cv2.COLOR_BGR2GRAY)
+                    chars_train.append(digit_img)
+                    # chars_label.append(1)
+                    chars_label.append(index)
+            chars_train = list(map(img_recognition.deskew, chars_train))
+            chars_train = img_recognition.preprocess_hog(chars_train)
+            # chars_train = chars_train.reshape(-1, 20, 20).astype(np.float32)
+            chars_label = np.array(chars_label)
+            print(chars_train.shape)
+            self.modelchinese.train(chars_train, chars_label)
+
+    def save_traindata(self):
+        if not os.path.exists("svm.dat"):
+            self.model.save("svm.dat")
+        if not os.path.exists("svmchinese.dat"):
+            self.modelchinese.save("svmchinese.dat")
+        def img_first_pre(self, car_pic_file):
+        """
+        :param car_pic_file: 图像文件
+        :return:已经处理好的图像文件 原图像文件
+        """
+        if type(car_pic_file) == type(""):
+            img = img_math.img_read(car_pic_file)
+        else:
+            img = car_pic_file
+
+        pic_hight, pic_width = img.shape[:2]
+        if pic_width > MAX_WIDTH:
+            resize_rate = MAX_WIDTH / pic_width
+            img = cv2.resize(img, (MAX_WIDTH, int(pic_hight * resize_rate)), interpolation=cv2.INTER_AREA)
+        # 缩小图片
+
+        blur = 5
+        img = cv2.GaussianBlur(img, (blur, blur), 0)
+        oldimg = img
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # 转化成灰度图像
+
+        Matrix = np.ones((20, 20), np.uint8)
+        img_opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, Matrix)
+        img_opening = cv2.addWeighted(img, 1, img_opening, -1, 0)
+        # 创建20*20的元素为1的矩阵 开操作，并和img重合
+
+        ret, img_thresh = cv2.threshold(img_opening, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        img_edge = cv2.Canny(img_thresh, 100, 200)
+        # Otsu’s二值化 找到图像边缘
+
+        Matrix = np.ones((4, 19), np.uint8)
+        img_edge1 = cv2.morphologyEx(img_edge, cv2.MORPH_CLOSE, Matrix)
+        img_edge2 = cv2.morphologyEx(img_edge1, cv2.MORPH_OPEN, Matrix)
+        return img_edge2, oldimg

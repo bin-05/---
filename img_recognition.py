5afef175d73fa23df87b40e2aa6179b77d1f187f
bin-05 <52254194+bin-05@users.noreply.github.com>
2022-01-06 17:11:38 +0800

    来自opencv的sanple,用于svm训练

--- img_recognition.py
+++ img_recognition.py
@@ -1,1 +1,19 @@
 
+import cv2
+import numpy as np
+from numpy.linalg import norm
+
+SZ = 20  # 训练图片长宽
+MAX_WIDTH = 1000  # 原始图片最大宽度
+Min_Area = 2000  # 车牌区域允许最大面积
+PROVINCE_START = 1000
+# 来自opencv的sample，用于svm训练
+def deskew(img):
+    m = cv2.moments(img)
+    if abs(m['mu02']) < 1e-2:
+        return img.copy()
+    skew = m['mu11'] / m['mu02']
+    M = np.float32([[1, skew, -0.5 * SZ * skew], [0, 1, 0]])
+    img = cv2.warpAffine(img, M, (SZ, SZ), flags=cv2.WARP_INVERSE_MAP | cv2.INTER_LINEAR)
+    return img
+
